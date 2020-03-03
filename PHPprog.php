e55008fc0e1b8c0d230d6565bae26d242c55eb51
Alex <montiel.ishino@gmail.com>
2020-03-03 18:51:54 +0000

    My first PHP prog

---
+++ PHPprog.php
@@ -0,0 +1,25 @@
+<?php
+
+#Author -me
+#print a string
+print "Hello wrold\n";
+
+#variable creation
+$DNA = 'ACTG';
+print $DNA."\n";
+
+#string concatenation
+$DNA2 = 'ATAGTCCTAGATATGTAGTA';
+print "Here are the original two DNA fragments:\n\n";
+print $DNA."\n";
+print $DNA2."\n\n";
+$DNA3 = "$DNA$DNA2";
+print "Here is the concatenation of the first two fragments (version 1):\n\n";
+print $DNA3."\n";
+
+#Transcribe DNA to RNA
+$RNA = $DNA3;
+$RNA = str_replace("T","U",$RNA);
+print "Here is the result of transcribing the DNA to RNA:\n\n";
+
+?>
